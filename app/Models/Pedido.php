eefaaef15b336221bea52a7762c908413fadcb2b
kaio-keinner <keinnerkaio@gmail.com.com>
2024-09-18 15:29:28 -0400

    fix: Corrige Rota

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -5,6 +5,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pedido extends Model
 {
-    protected $fillable = ['cliente', 'data_pedido', 'data_entrega', 'status'];
+    protected $fillable = ['nome', 'data_pedido', 'data_entrega', 'status'];
 }
 
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PedidoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/pedidos', [PedidoController::class, 'index'])->name('pedidos.index');
+Route::get('/', [PedidoController::class, 'index'])->name('pedidos.index');
 Route::post('/pedidos', [PedidoController::class, 'store'])->name('pedidos.store');
 Route::put('/pedidos/{id}', [PedidoController::class, 'update'])->name('pedidos.update');
 Route::delete('/pedidos/{id}', [PedidoController::class, 'destroy'])->name('pedidos.destroy');
